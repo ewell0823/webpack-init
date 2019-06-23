c245f7f03706309a34cbc15311e26fa3ba4579bf
sei <citron_color@ezweb.ne.jp>
2019-06-23 17:22:19 +0900

    install eslint-loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,11 @@
-const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
-const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
 
-const outputPath = path.resolve(__dirname, 'dist') // 絶対パスを生成
+const outputPath = path.resolve(__dirname, 'dist'); // 絶対パスを生成
 
 // console.log({outputPath})
 
@@ -15,14 +15,20 @@ module.exports = {
   // 成果物の出力先
   output: {
     filename: 'main.js',
-    path: outputPath
+    path: outputPath,
   },
   module: {
     rules: [
-      { 
+      {
+        enforce: 'pre', // 優先度を上げて実行してくれる
+        test: /\.jsx?/,
+        exclude: /node_modules/,
+        loader: 'eslint-loader',
+      },
+      {
         test: /\.jsx?$/, // testには拡張子を登録。モダンなシンタックスで書かれたのものはトランスパイルの対象になる。（js or jsx が対象になる）
         exclude: /node_modules/, // node_modulesのディレクトリ配下にあるものはトランスパイルの対象から除外する
-        loader: "babel-loader" // トランスパイルの実行
+        loader: 'babel-loader', // トランスパイルの実行
       },
       // {
       //   test: /\.css/,
@@ -36,48 +42,48 @@ module.exports = {
         use: [
           MiniCssExtractPlugin.loader,
           'css-loader',
-          'sass-loader'
-        ]
+          'sass-loader',
+        ],
       },
       {
         test: /\.(jpe?g|peg|gif|svg|ico|png)$/i,
         loader: 'url-loader',
         options: {
           limit: 2048,
-          name: './images/[name].[ext]'
-        }
+          name: './images/[name].[ext]',
+        },
       },
       {
         test: /\html$/,
-        loader: 'html-loader'
-      }
-    ]
+        loader: 'html-loader',
+      },
+    ],
   },
   devServer: {
-    contentBase: outputPath // デフォルト起動画面（index.html）の指定
+    contentBase: outputPath, // デフォルト起動画面（index.html）の指定
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html',
-      filename: './index.html'
+      filename: './index.html',
     }),
     new MiniCssExtractPlugin({
-      filename: '[name].[hash].css' // [name]はデフォルトでmainになる。[hash]はバンドルの度にユニークの値を設定する（ビルドする度のキャッシュの回避に有効）
-    })
+      filename: '[name].[hash].css', // [name]はデフォルトでmainになる。[hash]はバンドルの度にユニークの値を設定する（ビルドする度のキャッシュの回避に有効）
+    }),
   ],
   optimization: { // 「最適化」のこと。
     minimizer: [
-        new UglifyJsPlugin({ // https://github.com/mishoo/UglifyJS2
+      new UglifyJsPlugin({ // https://github.com/mishoo/UglifyJS2
         uglifyOptions: {
           compress: {
-            drop_console: true
-          }
-        }
+            drop_console: true,
+          },
+        },
       }),
-      new OptimizeCSSAssetsPlugin({})
-    ]
+      new OptimizeCSSAssetsPlugin({}),
+    ],
   },
-  devtool: 'eval-source-map'
-}
+  devtool: 'eval-source-map',
+};
 
 // npx webpack --mode development 実行コマンド
